3299ae7655642829f8ddc4665fd5d661355f30bb
NTZemp <45421131+NTZemp@users.noreply.github.com>
2018-11-28 16:26:06 +0100

    Add Comments

--- src/SwissTransport/Transport.cs
+++ src/SwissTransport/Transport.cs
@@ -6,6 +6,12 @@ namespace SwissTransport
 {
     public class Transport : ITransport
     {
+
+        /// <summary>
+        /// Asks Transport API for all Stations with query in the name
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns>Returns a Stations object with all the Stations returned by the API</returns>
         public Stations GetStations(string query)
         {
             var request = CreateWebRequest("http://transport.opendata.ch/v1/locations?query=" + query);
@@ -23,6 +29,12 @@ namespace SwissTransport
             return null;
         }
 
+        /// <summary>
+        /// Gets the Station identified by the Id and Station Name
+        /// </summary>
+        /// <param name="station"></param>
+        /// <param name="id"></param>
+        /// <returns>Returns a Stationboard object</returns>
         public StationBoardRoot GetStationBoard(string station, string id)
         {
             var request = CreateWebRequest("http://transport.opendata.ch/v1/stationboard?Station=" + station + "&id=" + id);
