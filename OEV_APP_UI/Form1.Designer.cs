3e96ad0952cf55d8c6cfa84a4cfe3fc9adb4d5bf
NTZemp <45421131+NTZemp@users.noreply.github.com>
2018-11-28 16:25:23 +0100

    Add New Windows Forms project

---
+++ OEV_APP_UI/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace OEV_APP_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lstTimeTable = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(12, 30);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(225, 22);
+            this.txtFrom.TabIndex = 1;
+            this.txtFrom.TabStop = false;
+            this.txtFrom.TextChanged += new System.EventHandler(this.GetSuggestions);
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(9, 9);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(33, 17);
+            this.lblFrom.TabIndex = 3;
+            this.lblFrom.Text = "Von";
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Enabled = false;
+            this.lblTo.Location = new System.Drawing.Point(390, 10);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(36, 20);
+            this.lblTo.TabIndex = 4;
+            this.lblTo.Text = "Nach";
+            this.lblTo.UseCompatibleTextRendering = true;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(393, 30);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(225, 22);
+            this.txtTo.TabIndex = 5;
+            this.txtTo.TextChanged += new System.EventHandler(this.GetSuggestions);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(665, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(136, 40);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Suchen";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lstTimeTable
+            // 
+            this.lstTimeTable.Location = new System.Drawing.Point(12, 57);
+            this.lstTimeTable.Name = "lstTimeTable";
+            this.lstTimeTable.Size = new System.Drawing.Size(789, 486);
+            this.lstTimeTable.TabIndex = 7;
+            this.lstTimeTable.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 555);
+            this.Controls.Add(this.lstTimeTable);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.txtFrom);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ÖV Fahrplan Preview";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListView lstTimeTable;
+    }
+}
+
---
+++ OEV_APP_UI/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwissTransport;
+using System.Windows.Forms;
+
+namespace OEV_APP_UI
+{
+    public partial class Form1 : Form
+    {
+
+        APIInterface APIInterface = new APIInterface();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtFrom.AutoCompleteMode = AutoCompleteMode.Suggest;
+            txtFrom.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            txtTo.AutoCompleteMode = AutoCompleteMode.Suggest;
+            txtTo.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            lstTimeTable.Columns.Add("Abfahrt");
+            lstTimeTable.Columns.Add("Ankunft");
+            lstTimeTable.Columns.Add("Dauer");
+            lstTimeTable.Columns.Add("Abfahrts Gleis");
+            GC.Collect();
+        }
+
+        private void GetSuggestions(object sender, EventArgs e)
+        {
+            TextBox currtextBox = (TextBox)sender;
+            if (currtextBox.Text.Length >= 4)
+            {
+                GC.Collect();
+                currtextBox.AutoCompleteCustomSource = APIInterface.GetSuggestions(currtextBox.Text);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            APIInterface.GetConnections(txtFrom.Text, txtTo.Text,ref lstTimeTable);
+        }
+
+
+    }
+}
