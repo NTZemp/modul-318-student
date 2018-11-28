4ce31eed29fe1f86b5fcf8c9251c91036ecbfd24
NTZemp <45421131+NTZemp@users.noreply.github.com>
2018-11-28 16:24:38 +0100

    Add API interface Class

---
+++ OEV_APP_UI/APIInterface.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SwissTransport;
+
+namespace OEV_APP_UI
+{
+    class APIInterface
+    {
+        public Transport transportAPI = new Transport();
+        private Stations stations;
+        private string Query;
+
+
+        public AutoCompleteStringCollection GetSuggestions(string query)
+        {
+            AutoCompleteStringCollection resColl = new AutoCompleteStringCollection();
+            if (Query == null||!query.Contains(Query))
+            {
+                stations = transportAPI.GetStations(query);
+                Query = query;
+                foreach (Station s in stations.StationList)
+                {
+                    resColl.Add(s.Name);
+                }
+            }
+            else
+            {
+                foreach (Station s in stations.StationList)
+                {
+                    if(s.Name.ToUpper().Contains(query.ToUpper()))
+                    {
+                        resColl.Add(s.Name);
+                    }
+                }
+            }
+
+
+            return resColl;
+        }
+
+        public ListView.ListViewItemCollection GetConnections(string From, string To, ref ListView listView)
+        {
+           
+        }
+    }
+}
