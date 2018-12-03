9ed2da210b81621dbeb6560e9953dcc2c46755f2
NTZemp <45421131+NTZemp@users.noreply.github.com>
2018-12-03 12:52:04 +0100

    Change MainForm Name

--- OEV_APP_UI/Program.cs
+++ OEV_APP_UI/Program.cs
@@ -16,7 +16,7 @@ namespace OEV_APP_UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new UI_Form());
         }
     }
 }
