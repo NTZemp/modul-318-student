df664df4105cd97bcbfa6405e2588e84f4421356
NTZemp <45421131+NTZemp@users.noreply.github.com>
2018-12-03 12:51:53 +0100

    Add new UX features

--- OEV_APP_UI/Form1.Designer.cs
+++ OEV_APP_UI/Form1.Designer.cs
@@ -1,6 +1,8 @@
-﻿namespace OEV_APP_UI
+﻿using System.Windows.Forms;
+
+namespace OEV_APP_UI
 {
-    partial class Form1
+    partial class UI_Form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,18 +30,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UI_Form));
             this.tcControl = new System.Windows.Forms.TabControl();
             this.tpConnections = new System.Windows.Forms.TabPage();
-            this.lstTimeTable = new System.Windows.Forms.ListView();
+            this.lstConnections = new System.Windows.Forms.ListView();
             this.btnSearch = new System.Windows.Forms.Button();
             this.txtFrom = new System.Windows.Forms.TextBox();
             this.txtTo = new System.Windows.Forms.TextBox();
             this.lblFrom = new System.Windows.Forms.Label();
             this.lblTo = new System.Windows.Forms.Label();
             this.tpTimeTable = new System.Windows.Forms.TabPage();
+            this.lstTimeTable = new System.Windows.Forms.ListView();
+            this.txtStation = new System.Windows.Forms.TextBox();
+            this.btnSearchStation = new System.Windows.Forms.Button();
+            this.lblStation = new System.Windows.Forms.Label();
+            this.dtpDepOrArr = new System.Windows.Forms.DateTimePicker();
+            this.txtTime = new System.Windows.Forms.TextBox();
             this.tcControl.SuspendLayout();
             this.tpConnections.SuspendLayout();
+            this.tpTimeTable.SuspendLayout();
             this.SuspendLayout();
             // 
             // tcControl
@@ -49,12 +58,15 @@
             this.tcControl.Location = new System.Drawing.Point(12, 12);
             this.tcControl.Name = "tcControl";
             this.tcControl.SelectedIndex = 0;
-            this.tcControl.Size = new System.Drawing.Size(890, 585);
+            this.tcControl.Size = new System.Drawing.Size(890, 609);
             this.tcControl.TabIndex = 8;
+            this.tcControl.SelectedIndexChanged += new System.EventHandler(this.ChangeAccpetBtn);
             // 
             // tpConnections
             // 
-            this.tpConnections.Controls.Add(this.lstTimeTable);
+            this.tpConnections.Controls.Add(this.txtTime);
+            this.tpConnections.Controls.Add(this.dtpDepOrArr);
+            this.tpConnections.Controls.Add(this.lstConnections);
             this.tpConnections.Controls.Add(this.btnSearch);
             this.tpConnections.Controls.Add(this.txtFrom);
             this.tpConnections.Controls.Add(this.txtTo);
@@ -63,30 +75,33 @@
             this.tpConnections.Location = new System.Drawing.Point(4, 25);
             this.tpConnections.Name = "tpConnections";
             this.tpConnections.Padding = new System.Windows.Forms.Padding(3);
-            this.tpConnections.Size = new System.Drawing.Size(882, 556);
+            this.tpConnections.Size = new System.Drawing.Size(882, 580);
             this.tpConnections.TabIndex = 0;
             this.tpConnections.Text = "Verbindungen";
             this.tpConnections.UseVisualStyleBackColor = true;
             // 
-            // lstTimeTable
+            // lstConnections
             // 
-            this.lstTimeTable.Location = new System.Drawing.Point(6, 63);
-            this.lstTimeTable.Name = "lstTimeTable";
-            this.lstTimeTable.Size = new System.Drawing.Size(867, 486);
-            this.lstTimeTable.TabIndex = 14;
-            this.lstTimeTable.UseCompatibleStateImageBehavior = false;
+            this.lstConnections.Location = new System.Drawing.Point(6, 119);
+            this.lstConnections.Name = "lstConnections";
+            this.lstConnections.Size = new System.Drawing.Size(867, 455);
+            this.lstConnections.TabIndex = 14;
+            this.lstConnections.UseCompatibleStateImageBehavior = false;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(659, 18);
+            this.btnSearch.Location = new System.Drawing.Point(747, 6);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(136, 40);
+            this.btnSearch.Size = new System.Drawing.Size(129, 52);
             this.btnSearch.TabIndex = 13;
             this.btnSearch.Text = "Suchen";
             this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // txtFrom
             // 
+            this.txtFrom.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtFrom.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.txtFrom.Location = new System.Drawing.Point(6, 36);
             this.txtFrom.Name = "txtFrom";
             this.txtFrom.Size = new System.Drawing.Size(225, 22);
@@ -96,6 +111,8 @@
             // 
             // txtTo
             // 
+            this.txtTo.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtTo.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.txtTo.Location = new System.Drawing.Point(387, 36);
             this.txtTo.Name = "txtTo";
             this.txtTo.Size = new System.Drawing.Size(225, 22);
@@ -124,27 +141,90 @@
             // 
             // tpTimeTable
             // 
+            this.tpTimeTable.Controls.Add(this.lstTimeTable);
+            this.tpTimeTable.Controls.Add(this.txtStation);
+            this.tpTimeTable.Controls.Add(this.btnSearchStation);
+            this.tpTimeTable.Controls.Add(this.lblStation);
             this.tpTimeTable.Location = new System.Drawing.Point(4, 25);
             this.tpTimeTable.Name = "tpTimeTable";
             this.tpTimeTable.Padding = new System.Windows.Forms.Padding(3);
-            this.tpTimeTable.Size = new System.Drawing.Size(882, 556);
+            this.tpTimeTable.Size = new System.Drawing.Size(882, 580);
             this.tpTimeTable.TabIndex = 1;
             this.tpTimeTable.Text = "Abfahrtstafel";
             this.tpTimeTable.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // lstTimeTable
             // 
+            this.lstTimeTable.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lstTimeTable.Location = new System.Drawing.Point(6, 119);
+            this.lstTimeTable.Name = "lstTimeTable";
+            this.lstTimeTable.Size = new System.Drawing.Size(867, 455);
+            this.lstTimeTable.TabIndex = 11;
+            this.lstTimeTable.UseCompatibleStateImageBehavior = false;
+            // 
+            // txtStation
+            // 
+            this.txtStation.Location = new System.Drawing.Point(6, 36);
+            this.txtStation.Name = "txtStation";
+            this.txtStation.Size = new System.Drawing.Size(301, 22);
+            this.txtStation.TabIndex = 10;
+            this.txtStation.TextChanged += new System.EventHandler(this.GetSuggestions);
+            // 
+            // btnSearchStation
+            // 
+            this.btnSearchStation.Location = new System.Drawing.Point(747, 6);
+            this.btnSearchStation.Name = "btnSearchStation";
+            this.btnSearchStation.Size = new System.Drawing.Size(129, 52);
+            this.btnSearchStation.TabIndex = 1;
+            this.btnSearchStation.Text = "Suchen";
+            this.btnSearchStation.UseVisualStyleBackColor = true;
+            // 
+            // lblStation
+            // 
+            this.lblStation.AutoSize = true;
+            this.lblStation.Location = new System.Drawing.Point(3, 16);
+            this.lblStation.Name = "lblStation";
+            this.lblStation.Size = new System.Drawing.Size(52, 17);
+            this.lblStation.TabIndex = 0;
+            this.lblStation.Text = "Station";
+            this.lblStation.UseWaitCursor = true;
+            // 
+            // dtpDepOrArr
+            // 
+            this.dtpDepOrArr.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dtpDepOrArr.CustomFormat = "MM/dd/yyyy";
+            this.dtpDepOrArr.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDepOrArr.Location = new System.Drawing.Point(9, 91);
+            this.dtpDepOrArr.Name = "dtpDepOrArr";
+            this.dtpDepOrArr.Size = new System.Drawing.Size(96, 22);
+            this.dtpDepOrArr.TabIndex = 9;
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(111, 91);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(64, 22);
+            this.txtTime.TabIndex = 15;
+            this.txtTime.Text = "hh:mm";
+            this.txtTime.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // UI_Form
+            // 
+            this.AcceptButton = this.btnSearch;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(918, 609);
+            this.ClientSize = new System.Drawing.Size(918, 633);
             this.Controls.Add(this.tcControl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
+            this.Name = "UI_Form";
             this.Text = "ÖV Fahrplan Preview";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tcControl.ResumeLayout(false);
             this.tpConnections.ResumeLayout(false);
             this.tpConnections.PerformLayout();
+            this.tpTimeTable.ResumeLayout(false);
+            this.tpTimeTable.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -153,13 +233,19 @@
 
         private System.Windows.Forms.TabControl tcControl;
         private System.Windows.Forms.TabPage tpConnections;
-        private System.Windows.Forms.ListView lstTimeTable;
+        private System.Windows.Forms.ListView lstConnections;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.TextBox txtFrom;
         private System.Windows.Forms.TextBox txtTo;
         private System.Windows.Forms.Label lblFrom;
         private System.Windows.Forms.Label lblTo;
         private System.Windows.Forms.TabPage tpTimeTable;
+        private TextBox txtStation;
+        private Button btnSearchStation;
+        private Label lblStation;
+        private ListView lstTimeTable;
+        private DateTimePicker dtpDepOrArr;
+        private TextBox txtTime;
     }
 }
 
--- OEV_APP_UI/Form1.cs
+++ OEV_APP_UI/Form1.cs
@@ -11,12 +11,13 @@ using System.Windows.Forms;
 
 namespace OEV_APP_UI
 {
-    public partial class Form1 : Form
+    public partial class UI_Form : Form
     {
 
         APIInterface APIInterface = new APIInterface();
+        string Placeholder = "hh:mm";
 
-        public Form1()
+        public UI_Form()
         {
             InitializeComponent();
         }
@@ -24,15 +25,12 @@ namespace OEV_APP_UI
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            txtFrom.AutoCompleteMode = AutoCompleteMode.Suggest;
-            txtFrom.AutoCompleteSource = AutoCompleteSource.CustomSource;
-            txtTo.AutoCompleteMode = AutoCompleteMode.Suggest;
-            txtTo.AutoCompleteSource = AutoCompleteSource.CustomSource;
-            lstTimeTable.View = View.Details;
-            lstTimeTable.Columns.Add("Abfahrt", lstTimeTable.Size.Width / 4);
-            lstTimeTable.Columns.Add("Ankunft", lstTimeTable.Size.Width / 4);
-            lstTimeTable.Columns.Add("Dauer", lstTimeTable.Size.Width / 4);
-            lstTimeTable.Columns.Add("Abfahrts Gleis", lstTimeTable.Size.Width / 4);
+            this.lstConnections.View = View.Details;
+            this.lstConnections.Columns.Add("Abfahrt", lstConnections.Size.Width / 4);
+            this.lstConnections.Columns.Add("Ankunft", lstConnections.Size.Width / 4);
+            this.lstConnections.Columns.Add("Dauer", lstConnections.Size.Width / 4);
+            this.lstConnections.Columns.Add("Gleis/Kante", lstConnections.Size.Width / 4);
+
             GC.Collect();
         }
 
@@ -48,10 +46,49 @@ namespace OEV_APP_UI
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-            
-            lstTimeTable.Items.AddRange(APIInterface.GetConnections(txtFrom.Text, txtTo.Text);)
+            if (txtFrom.Text == "" || txtTo.Text == "")
+                MessageBox.Show("Bitte alle Von und Bis Station angeben.", "Eingabefehler");
+            else
+                lstConnections.Items.AddRange(APIInterface.GetConnections(txtFrom.Text, txtTo.Text));
         }
 
+        private void ChangeAccpetBtn(object sender, EventArgs e)
+        {
+            if(tcControl.SelectedIndex == 0)
+            {
+                this.AcceptButton = btnSearch;
+            }
+            else
+            {
+                this.AcceptButton = btnSearchStation;
+            }
+        }
 
+        private void SetPlaceholder(ref TextBox textBox)
+        {
+
+        }
+
+        private void txtTime_DragLeave(object sender, DragEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "")
+            {
+                textBox.Text = Placeholder;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void Format(string ToFormat)
+        {
+            string[] split = ToFormat.Split(':');
+            if(split.Length == 2)
+            {
+               if()
+            }
+        }
     }
 }
